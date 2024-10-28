f9b29a6934b8ded8489b90e483e58ddbc5056c9c
James Paul Turner <james.turner@imperial.ac.uk>
2024-10-28 17:02:11 +0000

    Add new store_message command.

---
+++ main/management/commands/store_message.py
@@ -0,0 +1,27 @@
+"""Django management command to populate Kafka messages into application database."""
+
+from argparse import ArgumentParser
+from datetime import UTC, datetime
+from typing import Any
+
+from django.core.management.base import BaseCommand
+
+from ...models import DruncMessage
+
+
+class Command(BaseCommand):
+    """Store messages in the database."""
+
+    help = __doc__
+
+    def add_arguments(self, parser: ArgumentParser) -> None:
+        """Add commandline options."""
+        parser.add_argument("-t", "--topic", default="NO_TOPIC")
+        parser.add_argument("-m", "--message", default="NO_MESSAGE")
+
+    def handle(self, *args: Any, **kwargs: Any) -> None:  # type: ignore[misc]
+        """Command business logic."""
+        topic = kwargs["topic"]
+        message = kwargs["message"]
+        timestamp = datetime.now(tz=UTC)
+        DruncMessage.objects.create(topic=topic, timestamp=timestamp, message=message)
