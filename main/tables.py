997e78f0fd886f01277938f50c587bd21b667522
sahil raja <s.raja@imperial.ac.uk>
2024-11-11 16:00:42 +0000

    fixing time

--- main/tables.py
+++ main/tables.py
@@ -6,6 +6,6 @@ import django_tables2 as tables
 class DruncMessageTable(tables.Table):
     """Defines a Drunc Message Table for the data from the Kafka messages."""
 
-    timestamp = tables.DateTimeColumn(format="Y-m-d h:m:s")
+    timestamp = tables.DateTimeColumn(format="y-m-d , H:i")
     severity = tables.Column(verbose_name="Severity", orderable=True)
     message = tables.Column(verbose_name="Message", orderable=False)
