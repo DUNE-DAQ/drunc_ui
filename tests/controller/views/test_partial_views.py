5b809d3a79d34311bd61e69bd30dbdb4a683eeb7
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-11-12 16:56:16 +0000

    :white_check_mark: Add tests to partial views using arguments.

--- tests/controller/views/test_partial_views.py
+++ tests/controller/views/test_partial_views.py
@@ -40,3 +40,55 @@ class TestFSMView(LoginRequiredTest):
         assert response.context["events"] == [
             t.event for t in DruncFSM.get(event.target.name).current_state.transitions
         ]
+
+    def test_post_with_arguments(self, auth_client, mocker):
+        """Tests basic calls of view method."""
+        mock_send = mocker.patch("controller.views.partials.DruncFSM.send")
+
+        auth_client.post(
+            self.endpoint,
+            data={
+                "event": "",
+                "current_state": "none",
+                "arg1": "value1",
+                "arg2": "value2",
+            },
+        )
+        mock_send.assert_not_called()
+
+        auth_client.post(
+            self.endpoint,
+            data={
+                "event": "event",
+                "current_state": "none",
+                "arg1": "value1",
+                "arg2": "value2",
+            },
+        )
+        mock_send.assert_called_once_with("event", arg1="value1", arg2="value2")
+
+
+class TestDialogView(LoginRequiredTest):
+    """Test the process_manager.views.process_table view function."""
+
+    endpoint = reverse("controller:dialog")
+
+    def test_empty_post(self, auth_client):
+        """Tests basic calls of view method."""
+        response = auth_client.post(self.endpoint)
+        assert response.status_code == HTTPStatus.OK
+        assert response.context["current_state"] == "None"
+        assert response.context["event"] is None
+        assert response.context["args"] == ["arg1", "arg2", "arg3"]
+
+    def test_non_empty_post(self, auth_client):
+        """Tests basic calls of view method."""
+        event = "turn_left"
+        state = "confused"
+        response = auth_client.post(
+            self.endpoint, data={"event": event, "current_state": state}
+        )
+        assert response.status_code == HTTPStatus.OK
+        assert response.context["current_state"] == state
+        assert response.context["event"] == event
+        assert response.context["args"] == ["arg1", "arg2", "arg3"]
