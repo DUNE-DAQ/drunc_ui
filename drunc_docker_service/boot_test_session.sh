a80d0089d0e39690f22bde0f040efee4c037713d
Christopher Cave-Ayland <c.cave-ayland@imperial.ac.uk>
2024-11-14 12:06:05 +0000

    Adopt dunedaq image for controller support

--- drunc_docker_service/boot_test_session.sh
+++ drunc_docker_service/boot_test_session.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # setup dunedaq environment
-. /basedir/fddaq-v5.1.0-a9/env.sh
+. /basedir/NFD_DEV_241114_A9/env.sh
 
 # boot full test session
-drunc-process-manager-shell grpc://localhost:10054 boot test/config/test-session.data.xml test-session
+drunc-process-manager-shell grpc://localhost:10054 boot config/daqsystemtest/example-configs.data.xml local-2x3-config
