24a019e0868da61418a5babec28fb0caf82535b3
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-11-19 16:31:26 +0000

    :bug: Amend tests for test_partial_views

--- tests/controller/views/test_partial_views.py
+++ tests/controller/views/test_partial_views.py
@@ -4,7 +4,7 @@ from random import choice
 import pytest
 from django.urls import reverse
 
-from controller.fsm import DruncFSM
+from controller import fsm
 from controller.tables import FSMTable
 
 from ...utils import LoginRequiredTest
@@ -15,28 +15,34 @@ class TestFSMView(LoginRequiredTest):
 
     endpoint = reverse("controller:state_machine")
 
-    def test_empty_post(self, auth_client):
+    def test_empty_post(self, auth_client, mocker):
         """Tests basic calls of view method."""
+        mock_state = mocker.patch("controller.controller_interface.get_fsm_state")
+        mock_state.return_value = "initial"
+
         response = auth_client.post(self.endpoint)
         assert response.status_code == HTTPStatus.OK
         table = response.context["table"]
         assert isinstance(table, FSMTable)
-        assert response.context["current_state"] == "None"
-        assert response.context["events"] == [
-            t.event for t in DruncFSM.get("none").current_state.transitions
-        ]
+        assert response.context["current_state"] == "initial"
+        assert response.context["events"] == fsm.STATES["initial"]
 
-    @pytest.mark.parametrize("state", DruncFSM.states)
-    def test_non_empty_post(self, state, auth_client):
+    @pytest.mark.parametrize("state", fsm.STATES.keys())
+    def test_non_empty_post(self, state, auth_client, mocker):
         """Tests basic calls of view method."""
-        event = choice(DruncFSM.get(state.name).current_state.transitions)
+        mock_state = mocker.patch("controller.controller_interface.get_fsm_state")
+        mocker.patch("controller.controller_interface.send_event")
+
+        event = choice(fsm.STATES[state])
+        target = fsm.EVENTS[event]
+
+        mock_state.side_effect = [state, target]
+
         response = auth_client.post(
-            self.endpoint, data={"event": event.event, "current_state": state.name}
+            self.endpoint, data={"event": event, "current_state": state}
         )
         assert response.status_code == HTTPStatus.OK
         table = response.context["table"]
         assert isinstance(table, FSMTable)
-        assert response.context["current_state"] == event.target.name
-        assert response.context["events"] == [
-            t.event for t in DruncFSM.get(event.target.name).current_state.transitions
-        ]
+        assert response.context["current_state"] == target
+        assert response.context["events"] == fsm.STATES[target]
