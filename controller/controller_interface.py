905dc1dce6ba41192ad300834860a75e012b89fb
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-11-20 05:59:50 +0000

    :sparckles: Cache the controller URL.

--- controller/controller_interface.py
+++ controller/controller_interface.py
@@ -1,5 +1,7 @@
 """Module providing functions to interact with the drunc controller."""
 
+import functools
+
 from django.conf import settings
 from drunc.connectivity_service.client import ConnectivityServiceClient
 from drunc.controller.controller_driver import ControllerDriver
@@ -8,6 +10,7 @@ from drunc.utils.utils import get_control_type_and_uri_from_connectivity_service
 from druncschema.request_response_pb2 import Description
 
 
+@functools.cache
 def get_controller_uri() -> str:
     """Find where the root controller is running via the connectivity service.
 
