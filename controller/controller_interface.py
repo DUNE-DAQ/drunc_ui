af740867efe04620846b14b0e007494f6cb5aa9e
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-11-21 11:24:36 +0000

    :sparkles: Add process_arguments to be able to submit events.

--- controller/controller_interface.py
+++ controller/controller_interface.py
@@ -1,14 +1,34 @@
 """Module providing functions to interact with the drunc controller."""
 
 import functools
+from enum import IntEnum
+from typing import Any
 
 from django.conf import settings
 from drunc.connectivity_service.client import ConnectivityServiceClient
 from drunc.controller.controller_driver import ControllerDriver
+from drunc.utils.grpc_utils import pack_to_any
 from drunc.utils.shell_utils import create_dummy_token_from_uname
 from drunc.utils.utils import get_control_type_and_uri_from_connectivity_service
-from druncschema.controller_pb2 import FSMCommand, FSMResponseFlag
+from druncschema.controller_pb2 import Argument, FSMCommand, FSMResponseFlag
+from druncschema.generic_pb2 import bool_msg, float_msg, int_msg, string_msg
 from druncschema.request_response_pb2 import Description
+
+
+class Presence(IntEnum):
+    """Enum to represent the presence of an argument."""
+
+    MANDATORY = 0
+    OPTIONAL = 1
+
+
+class FieldType(IntEnum):
+    """Enum to represent the type of an argument."""
+
+    INT = 0
+    FLOAT = 1
+    STRING = 2
+    BOOL = 3
 
 
 @functools.cache
@@ -47,12 +67,15 @@ def get_fsm_state() -> str:
     return get_controller_status().data.state
 
 
-def send_event(event: str, **kwargs: dict[str, str]) -> FSMResponseFlag:
+def send_event(  # type: ignore[misc]
+    event: str,
+    arguments: dict[str, Any],
+) -> FSMResponseFlag:
     """Send an event to the controller.
 
     Args:
         event: The event to send.
-        **kwargs: The arguments for the event.
+        arguments: The arguments for the event.
 
     Returns:
         FSMResponseFlag: The flag returned by the controller. 0 if the event was
@@ -60,11 +83,13 @@ def send_event(event: str, **kwargs: dict[str, str]) -> FSMResponseFlag:
     """
     controller = get_controller_driver()
     controller.take_control()
-    command = FSMCommand(command_name=event, arguments=kwargs)
+    command = FSMCommand(
+        command_name=event, arguments=process_arguments(event, arguments)
+    )
     return controller.execute_fsm_command(command).flag
 
 
-def get_arguments(event: str) -> list[str]:
+def get_arguments(event: str) -> list[Argument]:
     """Get the arguments required to run an event.
 
     Args:
@@ -79,3 +104,35 @@ def get_arguments(event: str) -> list[str]:
     )
 
     return command.arguments
+
+
+def process_arguments(  # type: ignore[misc]
+    event: str,
+    arguments: dict[str, Any],
+) -> dict[str, Any]:
+    """Process the arguments for an event.
+
+    Args:
+        event: The event to process.
+        arguments: The arguments to process.
+
+    Returns:
+        dict: The processed arguments in a form compatible with the protobuf definition.
+    """
+    valid_args = get_arguments(event)
+    processed = {}
+    for arg in valid_args:
+        if arg.name not in arguments or arguments[arg.name] is None:
+            continue
+
+        match arg.type:
+            case FieldType.INT:
+                processed[arg.name] = pack_to_any(int_msg(value=arguments[arg.name]))
+            case FieldType.FLOAT:
+                processed[arg.name] = pack_to_any(float_msg(value=arguments[arg.name]))
+            case FieldType.STRING:
+                processed[arg.name] = pack_to_any(string_msg(value=arguments[arg.name]))
+            case FieldType.BOOL:
+                processed[arg.name] = pack_to_any(bool_msg(value=arguments[arg.name]))
+
+    return processed
