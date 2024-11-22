658083c677f13ecfd1a28d8da05348bd61bc87bb
TinyMarsh <ranismyth@gmail.com>
2024-11-22 17:00:34 +0000

    Use Daphne for ASGI server to handle async stuff

--- drunc_ui/settings/settings.py
+++ drunc_ui/settings/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS: list[str] = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -69,6 +70,7 @@ TEMPLATES = [
     },
 ]
 
+ASGI_APPLICATION = "drunc_ui.asgi.application"
 WSGI_APPLICATION = "drunc_ui.wsgi.application"
 
 
