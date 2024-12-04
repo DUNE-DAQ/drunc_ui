0e0430c68eadd1eecc20f253f38cb1fef4140a06
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-12-04 10:57:33 +0000

    :bug: Exclude tests/__init__.py from mypy.

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,6 +3,5 @@
 import signal
 
 # Windows does not have SIGHUP, so we define it here to avoid errors during tests
-# We check if the attribute exists to avoid errors during type checking
 if not hasattr(signal, "SIGHUP"):
     signal.SIGHUP = 1  # type: ignore[attr-defined]
