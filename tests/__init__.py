338c77418747a09c6bb50805a8b68de3068b01d9
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-12-04 10:37:28 +0000

    :bug: Manually add SIGHUP attr during tests, again.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,8 @@
 """Unit tests for drunc_ui."""
 
-import platform
 import signal
 
 # Windows does not have SIGHUP, so we define it here to avoid errors during tests
-if platform.system() == "Windows":
+# We check if the attribute exists to avoid errors during type checking
+if not hasattr(signal, "SIGHUP"):
     signal.SIGHUP = 1  # type: ignore[attr-defined]
