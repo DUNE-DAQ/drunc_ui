271bdc677fa22c4207de0997cef91d084fbdda3c
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2024-12-04 11:02:03 +0000

    :bug: Ignore all type checks on that line.

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,4 +4,4 @@ import signal
 
 # Windows does not have SIGHUP, so we define it here to avoid errors during tests
 if not hasattr(signal, "SIGHUP"):
-    signal.SIGHUP = 1  # type: ignore[attr-defined]
+    signal.SIGHUP = 1  # type: ignore
