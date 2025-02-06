ada6e61cb206104706cd8fd401cd477bc647343a
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2025-02-06 13:06:51 +0000

    :sparkles: Add session-manager related urls.

--- controller/urls.py
+++ controller/urls.py
@@ -11,11 +11,22 @@ partial_urlpatterns = [
     path("dialog", partials.dialog, name="dialog"),
     path("app_tree_summary", partials.app_tree_view_summary, name="app_tree_summary"),
     path("app_tree_table", partials.app_tree_view_table, name="app_tree_table"),
+    path(
+        "active_sessions_table",
+        partials.active_sessions_table_view,
+        name="active_sessions_table",
+    ),
+    path(
+        "available_config",
+        partials.available_configs_table_view,
+        name="available_config",
+    ),
 ]
 
 urlpatterns = [
     path("", pages.index, name="index"),
     path("app_tree", pages.app_tree_view, name="app_tree"),
     path("ers_logs", pages.ers_logs, name="ers_logs"),
+    path("sessions", pages.session_manager_view, name="session_manager"),
     path("partials/", include(partial_urlpatterns)),
 ]
