c385fc5a92ea08532de5bda61d33106872bb243f
Diego Alonso Alvarez <d.alonso-alvarez@imperial.ac.uk>
2025-08-12 17:26:13 +0100

    :white_check_mark: Fix tests, once more.

--- tests/process_manager/test_url.py
+++ tests/process_manager/test_url.py
@@ -1,16 +1,13 @@
-from django.test import TestCase
+from django.urls import Resolver404, resolve
+from pytest import raises
 
 
-class TestURLs(TestCase):
-    """Tests for the URLs in the process_manager app."""
+def test_boot_process_url_not_available():
+    """Test that the boot_process URL is not included when DEBUG is False.
 
-    def test_boot_process_url_not_available(self):
-        """Test that the boot_process URL is not included when DEBUG is False.
-
-        NOTE That tests are always run with DEBUG=False regardless of the actual
-        setting, so this test is to ensure the view does not expose debug information
-        in production.
-        """
-        from process_manager import urls
-
-        self.assertNotIn("boot_process", [url.pattern.name for url in urls.urlpatterns])
+    NOTE That tests are always run with DEBUG=False regardless of the actual
+    setting, so this test is to ensure the view does not expose debug information
+    in production.
+    """
+    with raises(Resolver404):
+        resolve("process_manager:boot_process")
